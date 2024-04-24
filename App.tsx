2eed1033186c140018bddd274aabcddf79988937
Oleg Khristenk <olegkhristenko@gmail.com>
2024-04-24 12:08:57 +0300

    Installed nativewind and tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,23 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+        <View className="flex-1 items-center justify-center bg-neutral-800">
+            <Text className="text-neutral-100">Open up App.tsx to start working on your app!</Text>
+            <Text>trregfdg</Text>
             <StatusBar style="auto" />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25252b',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: '#fff',
-    },
-});
